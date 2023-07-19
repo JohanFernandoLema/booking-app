95e7907ca0c602c084fa16a54567eb91bca8f320
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-19 12:32:10 -0400

    mongoose dependency added for db connection

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express'
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 8000
+dotenv.config()
 
 app.listen(port, () => {
   console.log('App connected successfully')

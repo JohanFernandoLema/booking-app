ec0c697e1b393018ab442a85ac9abb4bb794e121
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-19 12:57:49 -0400

    evn link modificated - was not getting connected to online db

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from 'express'
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 const app = express()
 const port = 8000
 dotenv.config()
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO)
+    console.log('Connected to mongoDB')
+  } catch (error) {
+    throw error
+  }
+}
+
 app.listen(port, () => {
+  connectDB()
   console.log('App connected successfully')
 })

c26eebf8035b28e8131aca9dabca9e8eed79998c
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-20 11:10:55 -0400

    Room and user model created

--- models/room.js
+++ models/room.js
@@ -1,9 +1,2 @@
-import express from 'express'
 
-const router = express.Router()
 
-// Creating home page
-// We use req when the end-user ask something
-// We use res so the server send info regarding the req the client did
-
-export default router

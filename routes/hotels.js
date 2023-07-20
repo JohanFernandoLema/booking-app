cac00130fcc53894d526abea019d9602be1ca1e0
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-20 09:36:29 -0400

    hotels module is being created by adding crud operations

---
+++ routes/hotels.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const router = express.Router()
+
+// Creating home page
+// We use req when the end-user ask something
+// We use res so the server send info regarding the req the client did
+
+// CREATE
+router.post('/', (req, res) => {})
+// UPDATE
+// DELETE
+// GET
+// GET ALL
+
+export default router

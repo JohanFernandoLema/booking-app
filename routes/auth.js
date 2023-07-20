7803e651afee6fee16ca54f7c5a92828234fbec5
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-20 09:26:50 -0400

    API for authentication created and linked towards main index file

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,11 @@ const router = express.Router()
 // We use req when the end-user ask something
 // We use res so the server send info regarding the req the client did
 router.get('/', (req, res) => {
-  res.send('Hi my firend')
+  res.send('Auth endpoint')
+})
+
+router.get('/register', (req, res) => {
+  res.send('Register Endpoint')
 })
 
 export default router

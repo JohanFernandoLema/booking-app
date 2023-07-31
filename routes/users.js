e5600f6b3f122aad3076eef7b734bf1745a3ce42
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-31 14:54:06 -0400

    verification in order to find out if a simple user or admin is logged in added

--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,24 @@ import {
   getUser,
   getAllUsers,
 } from '../controllers/user.js'
-import { verifyToken, verifyUser } from '../utils/verifyToken.js'
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js'
 
 const router = express.Router()
 
-// CHECK IF USER EXISTS
-router.get('/checkauthentication', verifyToken, (req, res, next) => {
-  res.send('Hello user you logged in')
-})
+// // CHECK IF USER EXISTS
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//   res.send('Hello user you logged in')
+// })
 
-// CHECK IF USER EXISTS SO THEY CAN DELETE THEIR OWN PROFILE
-router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-  res.send('Hello user you logged in, you can delete your account')
-})
+// // CHECK IF USER EXISTS SO THEY CAN DELETE THEIR OWN PROFILE
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//   res.send('Hello user you logged in, you can delete your account')
+// })
+
+// // CHECK IF USER IS ADMIN
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//   res.send('Hello admin you have the power')
+// })
 
 // CREATE
 router.post('/', createUser)

4e0aceee6027cfbbe25c25f632d10f973ebc5bb3
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-31 10:53:41 -0400

    token created in order to check if user can delete his/her account

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,19 @@ import {
   getUser,
   getAllUsers,
 } from '../controllers/user.js'
+import { verifyToken, verifyUser } from '../utils/verifyToken.js'
 
 const router = express.Router()
+
+// CHECK IF USER EXISTS
+router.get('/checkauthentication', verifyToken, (req, res, next) => {
+  res.send('Hello user you logged in')
+})
+
+// CHECK IF USER EXISTS SO THEY CAN DELETE THEIR OWN PROFILE
+router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+  res.send('Hello user you logged in, you can delete your account')
+})
 
 // CREATE
 router.post('/', createUser)
